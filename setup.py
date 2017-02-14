198ce052a48c1a30e2663b8cf165b3e37777b4c2
Taro Kiritani <taro.kiritani@epfl.ch>
2017-02-14 23:45:42 +0100

    Changed version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'tiffstack2avi',
   packages = [], # this must be the same as the name above
-  version = '1.0.1',
+  version = '1.0.2',
   description = 'A small python wrapper for ffmpeg',
   author = 'Taro Kiritani',
   author_email = 'taro.kiritani@epfl.ch',
   url = 'https://github.com/tarokiritani/tiffstack2avi', # use the URL to the github repo
-  download_url = 'https://github.com/tarokiritani/tiffstack2avi/tarball/1.0.1', # I'll explain this in a second
+  download_url = 'https://github.com/tarokiritani/tiffstack2avi/tarball/1.0.2', # I'll explain this in a second
   keywords = ['ffmpeg', 'python', 'wrapper'], # arbitrary keywords
   classifiers = [],
 )
