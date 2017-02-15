e5f6ea902b7a84bc09f6eeb0126759c97c3bf73c
Taro Kiritani <taro.kiritani@epfl.ch>
2017-02-15 15:29:33 +0100

    Tries to show readme on pypi

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@
 
 from distutils.core import setup
 
+try:
+    import pypandoc
+    long_description=pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description=open('README.md').read()
+
 setup(
   name = 'tiffstack2avi',
   packages = [],
-  version = '1.0.6',
+  version = '1.0.7',
   py_modules = ['tiffstack2avi'],
   description = 'A small python wrapper for ffmpeg',
   author = 'Taro Kiritani',
   author_email = 'taro.kiritani@epfl.ch',
   url = 'https://github.com/tarokiritani/tiffstack2avi',
-  download_url = 'https://github.com/tarokiritani/tiffstack2avi/tarball/1.0.6',
+  download_url = 'https://github.com/tarokiritani/tiffstack2avi/tarball/1.0.7',
   keywords = ['ffmpeg', 'python', 'wrapper'],
   classifiers = [],
   license='MIT',
