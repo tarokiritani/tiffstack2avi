51051bc57272878d7a944865ca0dc81a855605c6
Taro Kiritani <taro.kiritani@epfl.ch>
2017-02-15 15:34:23 +0100

    Adds long descrioption in setyp

--- setup.py
+++ setup.py
@@ -11,14 +11,15 @@ except(IOError, ImportError):
 setup(
   name = 'tiffstack2avi',
   packages = [],
-  version = '1.0.7',
+  version = '1.0.8',
   py_modules = ['tiffstack2avi'],
   description = 'A small python wrapper for ffmpeg',
   author = 'Taro Kiritani',
   author_email = 'taro.kiritani@epfl.ch',
   url = 'https://github.com/tarokiritani/tiffstack2avi',
-  download_url = 'https://github.com/tarokiritani/tiffstack2avi/tarball/1.0.7',
+  download_url = 'https://github.com/tarokiritani/tiffstack2avi/tarball/1.0.8',
   keywords = ['ffmpeg', 'python', 'wrapper'],
   classifiers = [],
   license='MIT',
+  long_description=open('docs/README.rst', 'rt').read()
 )
