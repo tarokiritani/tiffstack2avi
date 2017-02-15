74538940383519983171c864da27849d0d445654
Taro Kiritani <taro.kiritani@epfl.ch>
2017-02-15 16:48:04 +0100

    Fixes the setup for installation

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ from distutils.core import setup
 try:
     import pypandoc
     long_description=pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
+except(IOError, ImportError, RuntimeError):
     long_description=open('README.md').read()
 
 setup(
   name = 'tiffstack2avi',
   packages = [],
-  version = '1.1.3',
+  version = '1.1.4',
   py_modules = ['tiffstack2avi'],
   description = 'A small python wrapper for ffmpeg',
   author = 'Taro Kiritani',
   author_email = 'taro.kiritani@epfl.ch',
   url = 'https://github.com/tarokiritani/tiffstack2avi',
-  download_url = 'https://github.com/tarokiritani/tiffstack2avi/tarball/1.1.3',
+  download_url = 'https://github.com/tarokiritani/tiffstack2avi/tarball/1.1.4',
   keywords = ['ffmpeg', 'python', 'wrapper'],
   classifiers = [
                  'Programming Language :: Python :: 3.5'
