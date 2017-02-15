1e28c94bf820ab08460e16f494f22f70a7514908
Taro Kiritani <taro.kiritani@epfl.ch>
2017-02-15 16:20:35 +0100

    Shows readme.md in pypi

--- setup.py
+++ setup.py
@@ -11,18 +11,18 @@ except(IOError, ImportError):
 setup(
   name = 'tiffstack2avi',
   packages = [],
-  version = '1.1.0',
+  version = '1.1.1',
   py_modules = ['tiffstack2avi'],
   description = 'A small python wrapper for ffmpeg',
   author = 'Taro Kiritani',
   author_email = 'taro.kiritani@epfl.ch',
   url = 'https://github.com/tarokiritani/tiffstack2avi',
-  download_url = 'https://github.com/tarokiritani/tiffstack2avi/tarball/1.1.0',
+  download_url = 'https://github.com/tarokiritani/tiffstack2avi/tarball/1.1.1',
   keywords = ['ffmpeg', 'python', 'wrapper'],
   classifiers = [
                  'Programming Language :: Python :: 3.5'
                  
                  ],
   license='MIT',
-  long_description=open('README.md', 'rt').read()
+  long_description=long_description,
 )
